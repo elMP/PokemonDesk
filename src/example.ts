7d543a79c181ab09c64acf63ff7d3de0d83f6999
kneazle <kneazle@yandex.ru>
2021-05-13 19:21:09 +0300

    Typescript exercises

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,32 @@
-const tsSum = (arr: number[]) => arr.reduce((a: number, b: number) => a + b);
+type TypeFn = (first: string, second: string) => string;
 
-tsSum([1, 1]);
+const concat: TypeFn = (a, b) => a + b;
+concat('Hello ', 'World'); // -> Hello World;
+
+interface myTaskInterface {
+  howIDoIt: string;
+  simeArray: (string | number)[];
+  withData: [{ howIDoIt: string; simeArray: (string | number)[] }];
+}
+
+const myHometask: myTaskInterface = {
+  howIDoIt: 'I Do It Wel',
+  simeArray: ['string one', 'string two', 42],
+  withData: [{ howIDoIt: 'I Do It Wel', simeArray: ['string one', 23] }],
+};
+
+myHometask.howIDoIt = 'I dont understand';
+
+interface MyArray<T> {
+  [N: number]: T;
+  map<U>(fn: (el: T) => U): U[];
+  reduce<U>(fn: (accum: U, curValue: T) => U, initValue: U): U;
+  reduce<U>(fn: (accum: U, curValue: T) => U): U;
+}
+
+const tsArr: MyArray<number> = [5, 7, 42];
+
+tsArr.map((i) => i + 1);
+tsArr.map((i) => `$(i) + 1`);
+
+tsArr.reduce((i: number, j) => i + j);
