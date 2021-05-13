0f1da10a9af196af2d37f056d1cce85ba60a53d9
kneazle <kneazle@yandex.ru>
2021-05-13 03:51:01 +0300

    Add ESlint and Prettier

--- src/example.ts
+++ src/example.ts
@@ -1,6 +1,3 @@
-let tsString = "1";
-tsString = "2";
-
-let tsSum = (arr: number[]) => arr.reduce((a: number, b: number) => a + b);
+const tsSum = (arr: number[]) => arr.reduce((a: number, b: number) => a + b);
 
 tsSum([1, 1]);
